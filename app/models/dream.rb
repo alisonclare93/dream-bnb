53a17129be4341399fc0567f759f4ef29447d809
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-11-15 15:48:32 +0000

    Added has_many associations on Dream model

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,6 @@
 class Dream < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
   validates :name, :description, :price, :duration, :user_id, presence: true
 end
