3033fd9269e7a13f10347e675c2bb05629b94abb
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-11-15 15:35:54 +0000

    Added validations on Dream and User models

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,3 +1,4 @@
 class Dream < ApplicationRecord
   belongs_to :user
+  validates :name, :description, :price, :duration, :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :dreams
+  validates :first_name, :last_name, :email, :password, presence: true
 end
