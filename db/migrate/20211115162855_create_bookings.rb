e64a79af4a9d1bac7b70630ba887fac664bdf685
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-11-15 16:33:07 +0000

    Updated User model to use devise. Recreated Dream and Booking model that depended on it. Reintroduced same validations & associations.

--- db/migrate/20211115162855_create_bookings.rb
+++ db/migrate/20211115162855_create_bookings.rb
@@ -1,6 +1,6 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
+class CreateBookings < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :bookings do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
