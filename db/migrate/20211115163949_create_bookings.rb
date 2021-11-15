4bd7cb967822befe3d083d72e87854fe549763b2
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-11-15 16:50:13 +0000

    Fixed User model. Removed incorrect columns from Booking model.

--- db/migrate/20211115163949_create_bookings.rb
+++ db/migrate/20211115163949_create_bookings.rb
@@ -1,10 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :password
+      t.references :user, null: false, foreign_key: true
+      t.references :dream, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_162855) do
+ActiveRecord::Schema.define(version: 2021_11_15_164526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password"
+    t.bigint "user_id", null: false
+    t.bigint "dream_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["dream_id"], name: "index_bookings_on_dream_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "dreams", force: :cascade do |t|
@@ -44,9 +44,13 @@ ActiveRecord::Schema.define(version: 2021_11_15_162855) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "dreams"
+  add_foreign_key "bookings", "users"
   add_foreign_key "dreams", "users"
 end
