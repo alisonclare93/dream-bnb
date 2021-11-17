557104942a7c69932a5738517712c2cb1b950c87
Sofie Fiskerstrand <sofiepilskog@gmail.com>
2021-11-17 11:01:44 +0000

    small homepage changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,4 @@ class PagesController < ApplicationController
   def home
     @dreams = Dream.all
   end
-
-  #get top 5 dreams
-
-  def top_dreams
-    @top_dreams = Dream.where(rating: 5)
-  end
 end
