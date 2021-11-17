4044e5218ead1636dd2b15aeb55f8d895345e4f9
Alison Clare <alisonclare93@gmail.com>
2021-11-17 13:27:59 +0000

    created link to bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,7 +19,7 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = Booking.all
-    @bookings.select { |n| n.user == current_user }
+    @my_bookings = @bookings.select { |n| n.user == current_user }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   get 'account', to: 'user#show'
   devise_for :users
+  resources :users, only: :show do
+    resources :bookings, only: :index
+  end
   root to: 'pages#home'
   resources :dreams do
     resources :bookings, only: [:new, :create]
   end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
