b4c003766d57dc08ca879abc9dd81a4d60fc783e
Alison Clare <alisonclare93@gmail.com>
2021-11-18 12:29:53 +0000

    pundit setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
-    @dreams = Dream.all
+    @dreams = Dream.where("rating > ?", 3)
   end
 end
