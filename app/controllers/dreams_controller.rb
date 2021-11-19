ac0614ce6188f6f24861336ddec04012c7a3a843
Alison Clare <alisonclare93@gmail.com>
2021-11-19 12:02:28 +0000

    attempted to add to controller

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,5 +1,6 @@
 class DreamsController < ApplicationController
   before_action :find_dream, only: %i[show destroy edit]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     if params[:query].present?
